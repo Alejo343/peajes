597926324824522f7897bcdaf2d710e090874f6a
DAVID\david <davidalejangp@gmail.com>
2024-08-16 23:42:57 -0500

    Aplicaion funcional para modificar los peajes

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpOffice\PhpWord\TemplateProcessor;
+use Illuminate\Support\Facades\Storage;
+use Exception;
+
+
+class DocumentController extends Controller
+{
+    public function upload(Request $request)
+    {
+        $validatedData = $request->validate([
+            'option-toll' => 'required|string',
+            'code' => 'required|numeric',
+            'date' => 'required|date',
+            'time' => 'required|date_format:H:i',
+        ]);
+
+        // Convertir la fecha a formato legible para el documento
+        $validatedData['date'] = date('d/m/Y', strtotime($validatedData['date']));
+
+        // Definir la ruta del archivo en la carpeta 'storage/app/public/docs'
+        $filePath = 'public/docs/' . $validatedData['option-toll'] . '.docx';
+
+        // Crear una instancia de TemplateProcessor con la ruta del archivo
+        $newToll = new TemplateProcessor(Storage::path($filePath));
+
+        // Reemplazar los valores de la plantilla
+        $newToll->setValues(
+            array('code' => $validatedData['code'], 'date' => $validatedData['date'], 'time' => $validatedData['time'])
+        );
+
+        // Definir la ruta del archivo de salida
+        $outputFilePath = 'output/new' . $validatedData['option-toll'] . time() . '.docx';
+
+        // Guardar el archivo procesado
+        $newToll->saveAs(Storage::path($outputFilePath));
+
+        // Devolver el archivo generado como descarga al usuario
+        return response()->download(Storage::path($outputFilePath))->deleteFileAfterSend(true);
+    }
+}
