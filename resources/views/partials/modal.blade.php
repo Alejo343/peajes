521eff31ecc4bf8d2597031b728542ba5d022c34
DAVID\david <davidalejangp@gmail.com>
2024-09-14 20:02:24 -0500

    -Deshabilita modal

--- resources/views/partials/modal.blade.php
+++ resources/views/partials/modal.blade.php
@@ -1,15 +1,18 @@
 <!-- Modal -->
 <div id="myModal" class="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center hidden">
     <div class="flex flex-col max-w-md gap-2 p-6 rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
-        <h2 class="text-xl font-semibold leading-tight tracking-wide">Nombre a guardar</h2>
+        <h2 class="text-xl font-semibold leading-tight tracking-wide">Todavia no </h2>
         {{-- <form action="{{ route('upload') }}" method="POST" enctype="multipart/form-data" --}}
         <form action="" method="POST" enctype="multipart/form-data" class="spinner-form self-stretch space-y-3">
             @csrf
 
-            <p class="flex-1 dark:text-gray-600">Descripcion
+            <p class="flex-1 dark:text-gray-600">No esta habilitado todavia mor
+            </p>
+            <p class="flex-1 dark:text-gray-600">No sea golosa
             </p>
 
-            <div>
+
+            {{-- <div>
                 <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del
                     consecutivo</label>
                 <input type="number" id="name" name="name"
@@ -17,11 +20,12 @@
                     focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
                     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Nombre" required />
-            </div>
+            </div> --}}
 
             <div class="flex flex-col justify-center gap-3 mt-6 sm:flex-row">
-                <button class="px-6 py-2 rounded-sm" onclick="closeModal()">Cencelar</button>
-                <button class="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-600 dark:text-gray-50">Guardar</button>
+                <button type="button" class="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-600 dark:text-gray-50"
+                    onclick="closeModal()">Ya lo se</button>
+                {{-- <button class="px-6 py-2 rounded-sm shadow-sm dark:bg-violet-600 dark:text-gray-50">Guardar</button> --}}
             </div>
         </form>
     </div>
